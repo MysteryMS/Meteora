5868b2e8223b93c40c4e7cc94118a3d3395eedaa
MysteryMS <mysteryms@pop-os.localdomain>
2019-05-06 21:25:50 -0300

    traduções pra português e inglês

---
+++ src/commands/RewindCommand.js
@@ -0,0 +1,25 @@
+const Command = require('../structures/Command')
+const parse = require('parse-duration')
+
+class RewindCommand extends Command {
+    constructor() {
+        super('rewind')
+        this.aliases = ['voltar']
+        this.name = 'Rewind'
+        this.description = 'Volta a música em determinado tempo'
+        this.usage = '<tempo(s/m/h)'
+    }
+    async run(message, args, { t }) {
+        const lavaPlayer = this.client.calls.get(message.guild.id)
+        if (!this.client.lavalinkManager.has(message.guild.id)) {
+            return message.reply(t('commands:music.notPlaying'))
+        }
+        if (!lavaPlayer.nowPlaying) {
+            return message.reply(t('commands:music.notPlaying'))
+        }
+      if (parse(args[0]) > lavaPlayer.player.state.position) {
+          message.reply(t('commands:music.maxDuration'))
+      }
+      lavaPlayer.seek(parse[0] - lavaPlayer.player.state.position)
+    }
+}
