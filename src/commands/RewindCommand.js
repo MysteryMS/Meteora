14fc0812df9a3258db231a475c4a901a60741ef3
MysteryMS <pedropivapedropiva1@outlook.com>
2019-05-24 15:34:28 -0300

    oopsie

--- src/commands/RewindCommand.js
+++ src/commands/RewindCommand.js
@@ -2,26 +2,23 @@ const Command = require('../structures/Command')
 const parse = require('parse-duration')
 
 class RewindCommand extends Command {
-    constructor() {
-        super('rewind')
-        this.aliases = ['voltar']
-        this.name = 'Rewind'
-        this.description = 'Volta a música em um determinado tempo'
-        this.usage = '<tempo(s/m/h)'
+  constructor() {
+    super('rewind')
+    this.aliases = ['voltar']
+    this.name = 'Rewind'
+    this.description = 'Volta a música em um determinado tempo'
+    this.usage = '<tempo(s/m/h)'
+  }
+  async run(message, args, { t }) {
+    const lavaPlayer = this.client.calls.get(message.guild.id)
+    if (!this.client.lavalinkManager.manager.has(message.guild.id)) {
+      return message.reply(t('commands:music.notPlaying'))
     }
-    async run(message, args, { t }) {
-        const lavaPlayer = this.client.calls.get(message.guild.id)
-        if (!this.client.lavalinkManager.manager.has(message.guild.id)) {
-            return message.reply(t('commands:music.notPlaying'))
-        }
-        if (!lavaPlayer.nowPlaying) {
-            return message.reply(t('commands:music.notPlaying'))
-        }
-      if (parse(args[0]) > lavaPlayer.player.state.position) {
-          message.reply(t('commands:music.maxDuration'))
-      }
-      lavaPlayer.seek(parse[0] - lavaPlayer.player.state.position)
+    if (parse(args[0]) > lavaPlayer.player.state.position) {
+      message.reply(t('commands:music.maxDuration'))
     }
+    lavaPlayer.seek(parse[0] - lavaPlayer.player.state.position)
+  }
 }
 
 module.exports = RewindCommand
