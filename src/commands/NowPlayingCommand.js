72fcb2d1a28140fa245d51351f0aa27ca4c150b8
MysteryMS <pedropivapedropiva1@outlook.com>
2019-05-25 19:59:57 -0300

    cu da xuxa

--- src/commands/NowPlayingCommand.js
+++ src/commands/NowPlayingCommand.js
@@ -17,7 +17,7 @@ class NowPlayingCommand extends Command {
     }
     let embed = new RichEmbed()
       .setTitle(message.guild.name)
-      .addField('Música:', `[${this.client.calls.get(message.guild.id).nowPlaying.infotitle}](${this.client.calls.get(message.guild.id).nowPlaying.info.uri})`)
+      .addField('Música:', `[${this.client.calls.get(message.guild.id).nowPlaying.info.title}](${this.client.calls.get(message.guild.id).nowPlaying.info.uri})`)
       .addField('Volume:', this.client.calls.get(message.guild.id).player.state.volume + '/100')
       .addField('Autor:', this.client.calls.get(message.guild.id).nowPlaying.info.author)
       .addField('Duração:', pms(this.client.calls.get(message.guild.id).player.state.position) + '/' + pms(this.client.calls.get(message.guild.id).nowPlaying.info.length))
