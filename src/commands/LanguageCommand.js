1f224b0d6c9066cd45b2e91b1087f3575b7060c5
MysteryMS <pedropivapedropiva1@outlook.com>
2019-05-24 23:54:22 -0300

    oopsie

--- src/commands/LanguageCommand.js
+++ src/commands/LanguageCommand.js
@@ -10,7 +10,7 @@ class LanguageCommand extends Command {
   async run (message, args, { t }) {
     guild.findOne({ _id: message.guild.id }, async (err, database) => {
       if (err) console.log(err)
-        message.channel.send(new RichEmbed()
+      message.channel.send(new RichEmbed()
         .setTitle(t('commands:language.title'))
         .setDescription(t('commands:language.description'))
         .addField(t('commands:language.portugueseField'), '​​​')
@@ -19,27 +19,23 @@ class LanguageCommand extends Command {
       ).then(async (msg) => {
         await msg.react('🇧:regional_indicator_r:')
         await msg.react('🇺:regional_indicator_s:')
-    
-        const collector = msg.createReactionCollector((r, u) => (r.emoji.name === '🇧:regional_indicator_r:', '🇺:regional_indicator_s:') && (u.id !== client.user.id && u.id === message.author.id))
+        const collector = msg.createReactionCollector((r, u) => (r.emoji.name === '🇧:regional_indicator_r:', '🇺:regional_indicator_s:') && (u.id !== this.client.user.id && u.id === message.author.id))
 
         collector.on('collect', async r => {
           switch (r.emoji.name) {
             case '🇧:regional_indicator_r:':
-              await msg.edit(new Discord.RichEmbed().setDescription('Agora vou falar em português!').setColor('#42f445'))
+              await msg.edit(new RichEmbed().setDescription('Agora vou falar em português!').setColor('#42f445'))
               database.language = 'pt-BR'
               database.save()
               break
 
             case '🇺:regional_indicator_s:':
-                await msg.edit(new Discord.RichEmbed().setDescription('Now I\'ll speak english!').setColor('#db3939'))
-                database.language = 'en-US'
-
+              await msg.edit(new RichEmbed().setDescription('Now I\'ll speak english!').setColor('#db3939'))
+              database.language = 'en-US'
           }
         })
       })
-    }
+    })
   }
 }
-
-
 module.exports = LanguageCommand
