2f197a1d3724e29cb7eceb2d9d46b27f4fc91e10
MysteryMS <pedropivapedropiva1@outlook.com>
2019-05-27 01:15:00 -0300

    coisas arrumadas

--- src/commands/HelpCommand.js
+++ src/commands/HelpCommand.js
@@ -15,14 +15,14 @@ class HelpCommand extends Command {
     let util = new RichEmbed()
     let dev = new RichEmbed()
     let serverm = new RichEmbed()
-    this.client.commands.filter(c => c.category === 'Utils').forEach(a => util.setTitle(t('descriptions:helpEmbed.utilCateg')) && util.addField(a.name, t(`descriptions:descriptions.${a.label}`)) && util.setColor('#ffa730') && util.setFooter(t('descriptions:helpEmbed.argsFooter')))
+    this.client.commands.filter(c => c.category === 'Utils').forEach(a => util.setTitle(t('descriptions:helpEmbed.utilsCateg')) && util.addField(a.name, t(`descriptions:descriptions.${a.label}`)) && util.setColor('#ffa730') && util.setFooter(t('descriptions:helpEmbed.argsFooter')))
 
     this.client.commands.filter(c => c.category === 'Música').forEach(a => music.setTitle(t('descriptions:helpEmbed.musicCateg')) && music.addField(a.name, t(`descriptions:descriptions.${a.label}`)) && music.setColor('#4fa1ff') && music.setFooter(t('descriptions:helpEmbed.argsFooter')))
 
     this.client.commands.filter(c => c.category === 'Dev').forEach(a => dev.setTitle(t('descriptions:helpEmbed.devCateg')) && dev.addField(a.name, t(`descriptions:descriptions.${a.label}`)) && dev.setColor('#ff4648') && dev.setFooter(t('descriptions:helpEmbed.argsFooter')))
 
     this.client.commands.filter(c => c.category === 'Gerenciamento do' +
-      ' servidor').forEach(a => serverm.setTitle(t('descriptions:descriptions.helpEmbed.serverManagCateg')) && serverm.addField(a.name, t(`descriptions:descriptions.${a.label}`)) && serverm.setColor('#3eff92') && serverm.setFooter(t('descriptions:helpEmbed.argsFooter')))
+      ' servidor').forEach(a => serverm.setTitle(t('descriptions:helpEmbed.serverManagCateg')) && serverm.addField(a.name, t(`descriptions:descriptions.${a.label}`)) && serverm.setColor('#3eff92') && serverm.setFooter(t('descriptions:helpEmbed.argsFooter')))
 
     await message.author.send(util).catch(err => message.reply(t('commands:messages.cantDm')) && console.log(err))
     await message.author.send(dev)
