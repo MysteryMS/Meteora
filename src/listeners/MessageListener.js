669d1e904cbf2531f51e49cfe6f8913ca1137001
MysteryMS <pedropivapedropiva1@outlook.com>
2019-05-27 22:42:13 -0300

    oi

--- src/listeners/MessageListener.js
+++ src/listeners/MessageListener.js
@@ -6,36 +6,43 @@ class MessageListener extends EventListener {
     super('message')
   }
 
-  run (message, { t }) {
-    if (message.content.startsWith(`<@${this.client.user.id}`)) {
-      message.reply(t('descriptions:misc.botPing'))
-    }
-    if (message.author.bot) { return }
-
-    this.client.commands.forEach((command) => {
-      if (command.handle(message)) {}
-    })
-    Guild.findOne({ _id: message.guild.id }, function (err, database) {
-      if (!database) {
-        new Guild({
-          _id: message.guild.id,
-          name: message.guild.name,
-          counterOn: false,
-          counterChannel: null,
-          counterMessage: null,
-          wMessage: false,
-          wMessageMessage: null,
-          wMessageChannel: null,
-          leaveMessage: false,
-          leaveMessageMessage: null,
-          leaveMessageChannel: null,
-          language: 'pt-BR',
-          prefix: 'r>>'
-        }).save()
+  run (message) {
+    Guild.findOne({ _id: message.guild.id }, (err, database) => {
+      if (err) console.log(err)
+      let t = this.client.localeManager.getT(database.language)
+      if (message.content.startsWith(`<@${this.client.user.id}`)) {
+        message.reply(t('descriptions:misc.botPing'))
       }
-      if (err) return console.log(err)
+      if (message.author.bot) {
+        return
+      }
+
+      this.client.commands.forEach((command) => {
+        if (command.handle(message)) {
+        }
+      })
+      Guild.findOne({ _id: message.guild.id }, function (err, database) {
+        if (!database) {
+          new Guild({
+            _id: message.guild.id,
+            name: message.guild.name,
+            counterOn: false,
+            counterChannel: null,
+            counterMessage: null,
+            wMessage: false,
+            wMessageMessage: null,
+            wMessageChannel: null,
+            leaveMessage: false,
+            leaveMessageMessage: null,
+            leaveMessageChannel: null,
+            language: 'pt-BR',
+            prefix: 'r>>'
+          }).save()
+        }
+        if (err) return console.log(err)
+      })
+      message.channel.stopTyping()
     })
-    message.channel.stopTyping()
   }
 }
 
