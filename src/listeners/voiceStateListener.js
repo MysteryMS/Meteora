a0798edf4c59612a472d3064f7a2a5e6d8f09780
MysteryMS <pedropivapedropiva1@outlook.com>
2019-05-29 19:12:42 -0300

    nao funciona

--- src/listeners/voiceStateListener.js
+++ src/listeners/voiceStateListener.js
@@ -4,13 +4,17 @@ class voiceStateListener extends EventListener {
   constructor () {
     super('voiceStateUpdate')
   }
+
   async run (oldMember, newMember) {
     if (this.client.lavalinkManager.manager.has(oldMember.guild.id)) {
       console.log('xuxa 2')
       if (oldMember.voiceChannel.id === this.client.calls.get(oldMember.guild.id).player.channel) {
         console.log('xuxa meneghel 2')
+        if (newMember.voiceChannel.members.size === 1) {
+          console.log('xuxa 3')
+        }
         this.client.channels.get(this.client.calls.get(oldMember.guild.id).messageChannel).send('acabou a festa')
-        this.client.calls.get(oldMember.guild.id).player.stop()
+        // this.client.calls.get(oldMember.guild.id).player.stop()
       }
     }
   }
