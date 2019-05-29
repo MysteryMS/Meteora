5a6916de7184d43f32a6f1206e511d074dd80959
MysteryMS <pedropivapedropiva1@outlook.com>
2019-05-29 19:19:27 -0300

    steal me with a kiss

--- src/listeners/voiceStateListener.js
+++ src/listeners/voiceStateListener.js
@@ -10,10 +10,10 @@ class voiceStateListener extends EventListener {
       console.log('xuxa 2')
       if (oldMember.voiceChannel.id === this.client.calls.get(oldMember.guild.id).player.channel) {
         console.log('xuxa meneghel 2')
-        if (newMember.voiceChannel.members.size === 1) {
+        if (oldMember.guild.channels.get(this.client.calls.get(oldMember.guild.id)).members.size === 1) {
           console.log('xuxa 3')
         }
-        this.client.channels.get(this.client.calls.get(oldMember.guild.id).messageChannel).send('acabou a festa')
+        // this.client.channels.get(this.client.calls.get(oldMember.guild.id).messageChannel).send('acabou a festa')
         // this.client.calls.get(oldMember.guild.id).player.stop()
       }
     }
