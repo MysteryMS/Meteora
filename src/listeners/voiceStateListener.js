9b893162e27103d446791513ef6e6fe3a70d53cd
MysteryMS <pedropivapedropiva1@outlook.com>
2019-05-29 19:08:50 -0300

    errei o evento²

--- src/listeners/voiceStateListener.js
+++ src/listeners/voiceStateListener.js
@@ -7,7 +7,7 @@ class voiceStateListener extends EventListener {
   async run (oldMember, newMember) {
     if (this.client.lavalinkManager.manager.has(oldMember.guild.id)) {
       console.log('xuxa 2')
-      if (oldMember.id === this.client.calls.get(oldMember.guild.id).player.channel) {
+      if (oldMember.voiceChannel.id === this.client.calls.get(oldMember.guild.id).player.channel) {
         console.log('xuxa meneghel 2')
         this.client.channels.get(this.client.calls.get(oldMember.guild.id).messageChannel).send('acabou a festa')
         this.client.calls.get(oldMember.guild.id).player.stop()
