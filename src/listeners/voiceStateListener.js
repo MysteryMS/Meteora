1217def84cbee7e8547c2897c4b6743cfee2909e
MysteryMS <pedropivapedropiva1@outlook.com>
2019-05-29 19:25:09 -0300

    n aparece xuxa 3

--- src/listeners/voiceStateListener.js
+++ src/listeners/voiceStateListener.js
@@ -10,7 +10,7 @@ class voiceStateListener extends EventListener {
       console.log('xuxa 2')
       if (oldMember.voiceChannel.id === this.client.calls.get(oldMember.guild.id).player.channel) {
         console.log('xuxa meneghel 2')
-        if (oldMember.guild.channels.get(this.client.calls.get(oldMember.guild.id)).members.size === 1) {
+        if (oldMember.guild.channels.get(oldMember.voiceChannel.id).members.size === 1) {
           console.log('xuxa 3')
         }
         // this.client.channels.get(this.client.calls.get(oldMember.guild.id).messageChannel).send('acabou a festa')
