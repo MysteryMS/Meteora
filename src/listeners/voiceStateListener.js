65486f028cdadbb69c8cb4f8073652aec1312e94
MysteryMS <pedropivapedropiva1@outlook.com>
2019-05-29 19:28:35 -0300

    apareceu xuxa 3, amém!

--- src/listeners/voiceStateListener.js
+++ src/listeners/voiceStateListener.js
@@ -7,14 +7,11 @@ class voiceStateListener extends EventListener {
 
   async run (oldMember, newMember) {
     if (this.client.lavalinkManager.manager.has(oldMember.guild.id)) {
-      console.log('xuxa 2')
       if (oldMember.voiceChannel.id === this.client.calls.get(oldMember.guild.id).player.channel) {
-        console.log('xuxa meneghel 2')
         if (oldMember.guild.channels.get(oldMember.voiceChannel.id).members.size === 1) {
-          console.log('xuxa 3')
+          this.client.channels.get(this.client.calls.get(oldMember.guild.id).messageChannel).send('acabou a festa')
+          this.client.lavalinkManager.manager.leave()
         }
-        // this.client.channels.get(this.client.calls.get(oldMember.guild.id).messageChannel).send('acabou a festa')
-        // this.client.calls.get(oldMember.guild.id).player.stop()
       }
     }
   }
