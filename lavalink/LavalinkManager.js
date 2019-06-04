1f3f775faef3519951cc6a881e01d31f442f4a47
Pedro <38635037+MysteryMS@users.noreply.github.com>
2019-06-04 02:48:21 -0300

    Update LavalinkManager.js

--- lavalink/LavalinkManager.js
+++ lavalink/LavalinkManager.js
@@ -4,7 +4,7 @@ let nodes = require('../lavalinkNodes.json').hosts
 nodes = nodes.map(a => {
   let obj = {}
   obj.host = a
-  obj.port = '32770'
+  obj.port = '2333'
   obj.password = 'youshallnotpass'
   return obj
 })
