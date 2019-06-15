c1e251b6d900ea4840b4cf7dc0cf68988ae1ec62
MysteryMS <pedropivapedropiva1@outlook.com>
2019-06-15 16:59:59 -0300

    aee

--- src/commands/EvalCommand.js
+++ src/commands/EvalCommand.js
@@ -1,34 +1,33 @@
-const Command = require("../structures/Command")
+const Command = require('../structures/Command')
 
-const { inspect } = require("util")
-const { RichEmbed } = require("discord.js")
+const { inspect } = require('util')
+const { RichEmbed } = require('discord.js')
 
 class EvalCommand extends Command {
+  constructor () {
+    super('eval', ['evaluate'])
 
-	constructor () {
-		super("eval", ["evaluate"])
+    this.onlyOwner = true
+  }
 
-		this.onlyOwner = true
-	}
+  async run (message, args) {
+    const code = args.join(' ')
 
-	async run(message, args) {
-		const code = args.join(" ")
+    try {
+      // eslint-disable-next-line no-eval
+      const evaluated = await eval(code)
 
-		try {
-			const evaluated = await eval(code)
+      message.channel.send(inspect(evaluated, { depth: 0 }), { code: 'js' })
+    } catch (err) {
+      const embed = new RichEmbed()
+        .setTitle('Um erro inesperado ocorreu enquanto o comando estava sendo executado!')
+        .setDescription('```' + err.stack + '```')
+        .setColor('#FF0000')
+        .setTimestamp(new Date())
 
-			message.channel.send(inspect(evaluated, { depth: 0 }), { code: 'js' })
-		} catch (err) {
-			const embed = new RichEmbed()
-				.setTitle("Um erro inesperado ocorreu enquanto o comando estava sendo executado!")
-				.setDescription("```" + err.stack + "```")
-				.setColor("#FF0000")
-				.setTimestamp(new Date())
-
-			message.channel.send({ embed })
-		}
-	}
-
+      message.channel.send({ embed })
+    }
+  }
 }
 
 module.exports = EvalCommand
