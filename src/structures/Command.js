5033a07a0cc8728db01bf8b732b4e8d1202a665f
MysteryMS <pedropivapedropiva1@outlook.com>
2019-12-18 18:14:02 -0300

    fix

--- src/structures/Command.js
+++ src/structures/Command.js
@@ -33,7 +33,7 @@ class Command {
     guild.findOne({ _id: message.guild.id }, (err, database) => {
       let server = database
       let t = this.client.localeManager.getT(database.language)
-      const prefix = process.env.PREFIX
+      const prefix = database.prefix
       if (err) console.log(err)
       const withPrefixLabels = labels.map((label) => prefix + label)
 
