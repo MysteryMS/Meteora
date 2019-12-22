d37787626fee0e46d52ee1d6b744bf508b6055c7
Mystery <38635037+MysteryMS@users.noreply.github.com>
2019-12-21 22:49:14 -0300

    added locales to PlaylistCommand.js

--- src/commands/PlaylistCommand.js
+++ src/commands/PlaylistCommand.js
@@ -11,18 +11,18 @@ class PlaylistCommand extends Command {
   async run (message, args, server, { t }) {
     switch (args[0]) {
       case 'create':
-        if (server.playlist) return message.reply('Atuazlmente, servidores têm um limite de uma playlist.')
-        if (!args[1]) return message.reply('Especifique as músicas que você deseja adicionar.\n_A música precisa estar com o link no formato `youtu.be`_')
+        if (server.playlist) return message.reply(t('commands:playlist.onlyOne'))
+        if (!args[1]) return message.reply(t('commands:playlist.argueSongs'))
         let songs = args.slice(1)
         server.playlist = new Map().set('1', songs)
-        server.save().then(message.reply(`Playlist criada com sucesso!\nUtilize \`${server.prefix}playlist load 1\` para tocá-la `))
+        server.save().then(message.reply(t('commands:playlist.created', { prefix: server.prefix })))
         break
 
       case 'load':
         if (!message.member.voiceChannelID) return message.reply(t('commands:music.noVoiceChannel'))
-        if (!server.playlist) return message.reply('Poxa... parece que você não tem nenhuma playlist ainda. Que tal criar uma?!')
+        if (!server.playlist) return message.reply(t('commands:playlist.noPlaylist'))
         let theMusic = server.playlist.get(`${args[1]}`)
-        if (!theMusic) return message.reply('Hmmm... essa playlist não foi encontrada. Tem certeza que você colocou o número certo?!')
+        if (!theMusic) return message.reply(t('commands:playlist.notFound'))
         let player = await this.client.lavalinkManager.join(message.member.voiceChannel.id)
 
         player.loadPlaylist(theMusic)
@@ -45,18 +45,18 @@ class PlaylistCommand extends Command {
 
       case 'add':
         let thePlaylist = server.playlist.get(`${args[1]}`)
-        if (!thePlaylist) return message.reply('Hmmm... essa playlist não foi encontrada. Tem certeza que você colocou o número certo?!')
+        if (!thePlaylist) return message.reply(t('commands:playlist.notFound'))
         let newSongs = args.slice(2)
         newSongs.forEach(ab => thePlaylist.push(ab))
         server.playlist = new Map().set(`${args[1]}`, thePlaylist)
-        server.save().then(message.reply('Novas faixas adicionadas com sucesso!'))
+        server.save().then(message.reply(t('commands:playlist.newTracks')))
         break
 
       case 'remove':
         let playlistDelete = server.playlist.get(`${args[1]}`)
-        if (!playlistDelete) return message.reply('Hmmm... essa playlist não foi encontrada. Tem certeza que você colocou o número certo?!')
+        if (!playlistDelete) return message.reply(t('commands:playlist.notFound'))
         server.playlist = playlistDelete.delete(`${args[1]}`)
-        server.save().then(message.reply('Playlist deletada com sucesso!'))
+        server.save().then(message.reply(t('commands:playlist.deleted')))
     }
   }
 }
