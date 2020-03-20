dea32944055ed006add53bdce3769aa359939427
mystery <38635037+MysteryMS@users.noreply.github.com>
2020-03-20 17:56:03 -0300

    Fix auto-disconnect

--- src/listeners/voiceStateListener.js
+++ src/listeners/voiceStateListener.js
@@ -11,7 +11,7 @@ class voiceStateListener extends EventListener {
     const t = this.client.localeManager.getT(xuxa.language)
     if (this.client.lavalinkManager.manager.players.has(oldMember.guild.id)) {
       if (oldMember.voice.channelID === this.client.player.get(oldMember.guild.id).player.channel) {
-        if (oldMember.guild.channels.get(oldMember.voice.channelID).members.size === 1) {
+        if (oldMember.guild.channels.cache.get(oldMember.voice.channelID).members.size === 1) {
           this.client.channels.get(this.client.player.get(oldMember.guild.id).messageChannel).send(t('descriptions:structures.allMembersLeft', { channel: oldMember.voice.channel.name }))
           await this.client.lavalinkManager.manager.leave(oldMember.guild.id)
           this.client.lavalinkManager.manager.delete(oldMember.guild.id)
