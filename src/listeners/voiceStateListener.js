df18e3d9154162975f6697c0316c6b67b00d2b18
mystery <38635037+MysteryMS@users.noreply.github.com>
2020-03-20 18:38:37 -0300

    Fix auto-disconnect

--- src/listeners/voiceStateListener.js
+++ src/listeners/voiceStateListener.js
@@ -6,15 +6,15 @@ class voiceStateListener extends EventListener {
     super('voiceStateUpdate')
   }
 
-  async run (oldMember, newMember) {
-    const xuxa = await guild.findOne({ _id: oldMember.guild.id })
+  async run (oldState, newState) {
+    const xuxa = await guild.findOne({ _id: oldState.guild.id })
     const t = this.client.localeManager.getT(xuxa.language)
-    if (this.client.lavalinkManager.manager.players.has(oldMember.guild.id)) {
-      if (oldMember.voice.channelID === this.client.player.get(oldMember.guild.id).player.channel) {
-        if (oldMember.guild.channels.cache.get(oldMember.voice.channelID).members.size === 1) {
-          this.client.channels.get(this.client.player.get(oldMember.guild.id).messageChannel).send(t('descriptions:structures.allMembersLeft', { channel: oldMember.voice.channel.name }))
-          await this.client.lavalinkManager.manager.leave(oldMember.guild.id)
-          this.client.lavalinkManager.manager.delete(oldMember.guild.id)
+    if (this.client.lavalinkManager.manager.players.has(oldState.guild.id)) {
+      if (oldState.channelID === this.client.player.get(oldState.guild.id).player.channel) {
+        if (oldState.guild.channels.cache.get(oldState.channelID).members.size === 1) {
+          this.client.channels.cache.get(this.client.player.get(oldState.guild.id).messageChannel).send(t('descriptions:structures.allMembersLeft', { channel: oldState.channel.name }))
+          await this.client.lavalinkManager.manager.leave(oldState.guild.id)
+          this.client.lavalinkManager.manager.players.delete(oldState.guild.id)
         }
       }
     }
