c8285f81641e3117b34c035b0d716a104343f28b
mystery <38635037+MysteryMS@users.noreply.github.com>
2020-03-27 23:43:54 -0300

    Fixed auto-disconnect

--- src/listeners/voiceStateListener.js
+++ src/listeners/voiceStateListener.js
@@ -10,8 +10,8 @@ class voiceStateListener extends EventListener {
     const xuxa = await guild.findOne({ _id: oldState.guild.id })
     const t = this.client.localeManager.getT(xuxa.language)
     if (this.client.lavalinkManager.manager.players.has(oldState.guild.id)) {
-      if (oldState.channelID === this.client.player.get(oldState.guild.id).player.channel) {
-        if (oldState.guild.channels.cache.get(oldState.id).members.size === 1) {
+      if (oldState.channelID === this.client.lavalinkManager.manager.voiceStates.get(oldState.guild.id).channel_id) {
+        if (oldState.channel.members.size === 1) {
           this.client.channels.cache.get(this.client.player.get(oldState.guild.id).messageChannel).send(t('descriptions:structures.allMembersLeft', { channel: oldState.channel.name }))
           await this.client.lavalinkManager.manager.leave(oldState.guild.id)
           this.client.lavalinkManager.manager.players.delete(oldState.guild.id)
@@ -21,9 +21,9 @@ class voiceStateListener extends EventListener {
       }
     }
     if (oldState.member.id === '464304679128530954') {
-     // this.client.lavalinkManager.manager.players.delete(oldState.guild.id)
+      // this.client.lavalinkManager.manager.players.delete(oldState.guild.id)
       // this.client.player.delete(oldState.guild.id)
-     // this.client.lavalinkManager.manager.voiceServers.delete(oldState.guild.id)
+      // this.client.lavalinkManager.manager.voiceServers.delete(oldState.guild.id)
     }
   }
 }
