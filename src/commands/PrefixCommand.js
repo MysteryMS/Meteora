f86cd7a9dd106d90f8fc27ad61afff58c9c089f4
MysteryMS <38635037+MysteryMS@users.noreply.github.com>
2020-04-07 13:03:23 -0300

    meu senhor...

--- src/commands/PrefixCommand.js
+++ src/commands/PrefixCommand.js
@@ -5,6 +5,7 @@ class PrefixCommand extends Command {
     super('prefix')
     this.name = 'Prefix'
     this.category = 'server'
+    this.memberPermissions = ['MANAGE_SERVER']
   }
 
   async run (message, args, server, { t }) {
