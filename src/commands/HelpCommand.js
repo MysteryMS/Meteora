bff3d6fa971257e6f4c765fcb061dc8613230ba6
MysteryMS <38635037+MysteryMS@users.noreply.github.com>
2020-04-23 22:56:22 -0300

    removing beta category - hotfix
    it will be still available for users to test the commands, but will not longer show on embed
    i work on this later

--- src/commands/HelpCommand.js
+++ src/commands/HelpCommand.js
@@ -15,7 +15,6 @@ class HelpCommand extends Command {
       .addField(`${t('descriptions:helpEmbed.utilsCateg')} (${this.getCommandSize('util', this)})`, this.getCategory('util', server.prefix, this))
       .addField(`${t('descriptions:helpEmbed.musicCateg')} (${this.getCommandSize('music', this)})`, this.getCategory('music', server.prefix, this))
       .addField(`${t('descriptions:helpEmbed.botCateg')} (${this.getCommandSize('server')})`, this.getCategory('server', server.prefix))
-      .addField(`Beta (${this.getCommandSize('beta')})`, this.getCategory('beta', server.prefix))
       .setColor('#de3368')
       .setAuthor(t('descriptions:helpEmbed.author', { user: message.author.username }), message.author.displayAvatarURL())
     await message.author.send(embed).catch(() => message.channel.send(t('commands:messages.cantDm')))
