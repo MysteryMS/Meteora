84ea239ba89f7935efc25fe9abb2b5e506c584dd
MysteryMS <38635037+MysteryMS@users.noreply.github.com>
2020-09-07 21:16:06 -0300

    pintao

---
+++ src/main/kotlin/com/mystery/meteora/controller/LocaleController.kt
@@ -0,0 +1,16 @@
+package com.mystery.meteora.controller
+
+import com.mystery.meteora.controller.model.Guilds
+import org.litote.kmongo.KMongo
+import org.litote.kmongo.findOneById
+import org.litote.kmongo.getCollection
+import java.util.*
+
+  fun String.translate(config: Config, guildId: String, vararg params: Any): String {
+    val client = KMongo.createClient(config.config?.databaseConfig?.connectionUri!!)
+    val guild = client.getDatabase("meteora").getCollection<Guilds>().findOneById(guildId)
+    val locale = guild?.language?.split( '_')!!
+    val bundle = ResourceBundle.getBundle("commands", Locale(locale[0], locale[1]))
+    val string = bundle.getString(this)
+    return string.format(*params)
+  }
