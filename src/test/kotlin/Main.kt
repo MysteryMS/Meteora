fad4a91c05f8fa7ecafa6892e0ddad79e1d5e1c2
MysteryMS <38635037+MysteryMS@users.noreply.github.com>
2020-09-12 17:42:19 -0300

    Fix gradle things

--- src/test/kotlin/Main.kt
+++ src/test/kotlin/Main.kt
@@ -1,6 +1,7 @@
+import com.sedmelluq.discord.lavaplayer.tools.PlayerLibrary
 import kotlin.random.Random
 
 fun main() {
     val msgNum = List(1) { Random.nextInt(1, 3) }
-    println(msgNum[0])
+    return println(PlayerLibrary.VERSION)
 }
